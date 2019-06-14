c570faf699ee009a7231d0ce96a1eb5b32f2a12e
Miles Standoffish <toddnan@gmail.com>
2019-06-14 16:32:27 -0500

    Game object started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,23 @@
+
+let game = {
+    wordBank: ["Cameron", "Sloan", "Jeannie", "Edward Rooney", "Grace", "Abe Froman", "Ferris Bueller"],
+    currentWord: "",
+    guessesAllowed: 10,
+    guessesRemaining: 0,
+    numWins: 0,
+    
+    
+    initGame : function() {
+         this.numWins = document.getElementById('numWins');
+         this.guessesRemaining = document.getElementById('guessesRemaining');
+    }
+}
+
+game.initGame();
+
 document.onkeyup = function(event) {
     alert (event.key);
     
 
   }
+
