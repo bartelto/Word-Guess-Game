30fc31219c6d161aa74db7fa1858e9cdbf036312
Miles Standoffish <toddnan@gmail.com>
2019-06-14 17:37:56 -0500

    Some screen updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,60 @@
+//var instr = document.getElementById('instructions');
 
 let game = {
     wordBank: ["Cameron", "Sloan", "Jeannie", "Edward Rooney", "Grace", "Abe Froman", "Ferris Bueller"],
     currentWord: "",
+    displayedWord: "",
+    started: false,
     guessesAllowed: 10,
     guessesRemaining: 0,
     numWins: 0,
+    instructions: "",
     
     
-    initGame : function() {
-         this.numWins = document.getElementById('numWins');
-         this.guessesRemaining = document.getElementById('guessesRemaining');
-    }
-}
+    init: function() {
+        console.log("init function");
+        this.instructions = document.getElementById('instructions');
+        this.numWins = document.getElementById('numWins');
+        this.guessesRemaining = document.getElementById('guessesRemaining');
+        this.displayedWord = document.getElementById('displayedWord');
+    },
 
-game.initGame();
+    start: function() {
+        console.log("start function");
+        this.started = true;
+        this.instructions.textContent = "Type a letter to guess!"; // Clear instructions from screen
+        this.chooseWord();
+        this.guessesRemaining = this.guessesAllowed;
+    },
+
+    chooseWord: function() {
+        this.currentWord = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
+        console.log("current word: " + this.currentWord);
+        this.displayedWord.textContent = ('_').repeat(this.currentWord.length);
+        //this.displayedWord.textContent = "Todd";
+    },
+
+    guess: function(letter) {
+        console.log("guess: " + letter);
+        // Check if key is a letter
+
+    },
+
+    updateDisplayedWord: function() {
+
+    }
+
+};
+
+game.init();
+//instr.textContent = "hello";
 
 document.onkeyup = function(event) {
-    alert (event.key);
+    if (!game.started) {
+        game.start();
+    } else {
+        game.guess(event.key);
+    }
     
 
   }
